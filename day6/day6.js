d9d2d05083cfc3a7c1e6cf7c311c54e5fe88bcdb
AshishLNadoda <106494113+AshishLNadoda@users.noreply.github.com>
2023-03-18 23:12:45 +0530

    day 6

---
+++ day6/day6.js
@@ -0,0 +1,16 @@
+const num='6';
+let sqr=Math.sqrt(num);
+console.log(sqr);
+let x = Math.random() * 10;
+console.log(x);
+
+const today = new Date();
+console.log(today);
+
+
+const date = new Date("July 21, 1983");
+let day = date.getDate();
+console.log(day);
+console.log(today-day);
+console.log((today-day)/(1000*60*60*24));
+
---
+++ day6/readme.md
@@ -0,0 +1,3 @@
+in this we use some math operation in js 
+and we also use js date function by my own example
+thanxxxxx
